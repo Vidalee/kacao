e44d132fd1af8bc95e2532c29b678453fc7e91e2
Vivi <vivi@vivide.re>
2025-04-24 00:26:36 +0200

    feat: working limit by get messages

--- cmd/get/messages.go
+++ cmd/get/messages.go
@@ -14,7 +14,7 @@ import (
 )
 
 var messagesCmd = &cobra.Command{
-	Use:   "messages",
+	Use:   "messages <topic_name> [--limit <limit>]",
 	Short: "Get messages from a topic",
 	Long:  `Get messages from a topic`,
 	Run: func(command *cobra.Command, args []string) {
@@ -45,13 +45,21 @@ var messagesCmd = &cobra.Command{
 
 		committedListedOffsets, _ := adminClient.ListEndOffsets(ctx, args...)
 
+		var limitsByPartition = make(map[int32]int64)
+		var counterByPartition = make(map[int32]int64)
+		for _, listedOffset := range committedListedOffsets[args[0]] {
+			limitsByPartition[listedOffset.Partition] = 10
+			counterByPartition[listedOffset.Partition] = 0
+		}
+
 		var newOffsets kadm.Offsets = make(map[string]map[int32]kadm.Offset)
 		newOffsets[args[0]] = make(map[int32]kadm.Offset)
 		for _, listedOffset := range committedListedOffsets[args[0]] {
-			var offsetValue int64 = 0
+			var offsetValue int64 = listedOffset.Offset - limit
 
-			if listedOffset.Offset-limit > 0 {
-				offsetValue = listedOffset.Offset - limit
+			if offsetValue < 0 {
+				offsetValue = 0
+				limitsByPartition[listedOffset.Partition] = listedOffset.Offset
 			}
 
 			newOffsets[args[0]][listedOffset.Partition] = kadm.Offset{
@@ -75,7 +83,7 @@ var messagesCmd = &cobra.Command{
 			os.Exit(0)
 		}()
 
-		records := make([]kgo.Record, limit)
+		records := make([]kgo.Record, 0)
 
 		for {
 			fetches := cl.PollFetches(ctx)
@@ -85,20 +93,53 @@ var messagesCmd = &cobra.Command{
 
 			iter := fetches.RecordIter()
 
-			counterByPartition := make(map[int32]int64)
-			for i, _ := range committedListedOffsets[args[0]] {
-				counterByPartition[i] = 0
+			for !iter.Done() {
+				record := *iter.Next()
+				if counterByPartition[record.Partition] >= limitsByPartition[record.Partition] {
+					continue
+				}
+				counterByPartition[record.Partition]++
+				records = append(records, record)
 			}
 
-			for !iter.Done() {
-				slices.AppendSeq(records, iter.Next())
+			allCountersReached := true
+			for partition, counter := range counterByPartition {
+				if counter < limitsByPartition[partition] {
+					allCountersReached = false
+					break
+				}
+			}
+			if allCountersReached {
+				break
+			}
+		}
+
+		slices.SortFunc(records, func(a, b kgo.Record) int {
+			diff := a.Timestamp.Unix() - b.Timestamp.Unix()
+			if diff > 0 {
+				return -1
+			}
+			if diff < 0 {
+				return 1
+			}
+			return 0
+		})
+
+		if len(records) > int(limit) {
+			records = records[:limit]
+		}
+
+		for _, record := range records {
+			fmt.Printf("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s\n", record.Topic, record.Partition, record.Offset, string(record.Key), string(record.Value))
+			for _, header := range record.Headers {
+				fmt.Printf("Header: %s: %s\n", header.Key, string(header.Value))
 			}
 		}
 	},
 }
 
 func init() {
-	messagesCmd.Flags().Int64("limit", 10, "Limit the number of messages to get")
+	messagesCmd.Flags().Int64("limit", 10, "Limit the number of messages to get.")
 
 	getCmd.AddCommand(messagesCmd)
 }
